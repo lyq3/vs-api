7dafc3e9cab803625742c632e0a8b9f79fbd824e
罗云强 <99190092@qq.com>
2017-09-27 15:01:57 +0800

    ce

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,160 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+    <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
+        <Row type="flex" >
+            <Col :span="spanLeft" class="layout-menu-left">
+                <Menu active-name="1" theme="dark" width="auto">
+                    <div class="layout-logo-left">CNCT-API管理系统</div>
+                    <MenuItem name="1">
+                        <Icon type="ios-navigate" :size="iconSize"></Icon>
+                        <span class="layout-text">选项 1</span>
+                    </MenuItem>
+                    <MenuItem name="2">
+                        <Icon type="ios-keypad" :size="iconSize"></Icon>
+                        <span class="layout-text">选项 2</span>
+                    </MenuItem>
+                    <MenuItem name="3">
+                        <Icon type="ios-analytics" :size="iconSize"></Icon>
+                        <span class="layout-text">选项 3</span>
+                    </MenuItem>
+                </Menu>
+            </Col>
+            <Col :span="spanRight">
+                <div class="layout-header">
+                    <Button type="text" @click="toggleClick">
+                        <Icon type="navicon" size="32"></Icon>
+                    </Button>
+                    <v-navbar></v-navbar>
+                </div>
+                <div class="layout-breadcrumb">
+                    <Breadcrumb>
+                        <BreadcrumbItem href="#">首页</BreadcrumbItem>
+                        <BreadcrumbItem href="#">应用中心</BreadcrumbItem>
+                        <BreadcrumbItem>某应用</BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
+                <div class="layout-content">
+                    <div class="layout-content-main">
+                      <Col span="5">
+                    <Menu active-name="1-2" width="auto" :open-names="['1']">
+                        <Submenu name="1">
+                            <template slot="title">
+                                <Icon type="ios-navigate"></Icon>
+                                导航一
+                            </template>
+                            <MenuItem name="1-1">选项 1</MenuItem>
+                            <MenuItem name="1-2">选项 2</MenuItem>
+                            <MenuItem name="1-3">选项 3</MenuItem>
+                        </Submenu>
+                        <Submenu name="2">
+                            <template slot="title">
+                                <Icon type="ios-keypad"></Icon>
+                                导航二
+                            </template>
+                            <MenuItem name="2-1">选项 1</MenuItem>
+                            <MenuItem name="2-2">选项 2</MenuItem>
+                        </Submenu>
+                        <Submenu name="3">
+                            <template slot="title">
+                                <Icon type="ios-analytics"></Icon>
+                                导航三
+                            </template>
+                            <MenuItem name="3-1">选项 1</MenuItem>
+                            <MenuItem name="3-2">选项 2</MenuItem>
+                        </Submenu>
+                    </Menu>
+                </Col>
+                    </div>
+                </div>
+                <div class="layout-copy">
+                    2011-2016 &copy; TalkingData
+                </div>
+            </Col>
+        </Row>
+    </div>
 </template>
-
 <script>
-export default {
-  name: 'app'
-}
+  import navbar from './components/header/navbar.vue'
+    export default {
+        data () {
+            return {
+                spanLeft: 5,
+                spanRight: 19
+            }
+        },
+        components:{
+          'v-navbar':navbar
+        },
+        computed: {
+            iconSize () {
+                return this.spanLeft === 5 ? 14 : 24;
+            }
+        },
+        methods: {
+            toggleClick () {
+                if (this.spanLeft === 5) {
+                    this.spanLeft = 2;
+                    this.spanRight = 22;
+                } else {
+                    this.spanLeft = 5;
+                    this.spanRight = 19;
+                }
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .layout-breadcrumb{
+        padding: 10px 15px 0;
+    }
+    .layout-content{
+        min-height: 200px;
+        margin: 15px;
+        overflow: hidden;
+        background: #fff;
+        border-radius: 4px;
+    }
+    .layout-content-main{
+        padding: 10px;
+    }
+    .layout-copy{
+        text-align: center;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
+    }
+    .layout-menu-left{
+        background: #464c5b;
+        height: 800px;
+    }
+    .layout-header{
+        height: 60px;
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+    .layout-logo-left{
+        width: 90%;
+        line-height: 35px;
+        background: #5b6270;
+        border-radius: 3px;
+        margin: 15px auto;
+        color: #fff;
+        text-align: center;
+        font-size: 20px;
+    }
+    .layout-ceiling-main a{
+        color: #9ba7b5;
+    }
+    .layout-hide-text .layout-text{
+        display: none;
+    }
+    .ivu-col{
+        transition: width .2s ease-in-out;
+    }
 </style>
